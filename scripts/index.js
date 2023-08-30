71fccb8045228ece15b86b1c5ec95b5f8d88ff9b
XDRO <131299160+XDRO@users.noreply.github.com>
2023-08-30 09:30:57 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -29,7 +29,13 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseModal = document.querySelector("#profile-close-modal");
 
 profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal_opened");
 });
+
+profileCloseModal.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
+
