3d6d8e50f7d37883e5f7476937c1edcab58e43a2
XDRO <131299160+XDRO@users.noreply.github.com>
2023-08-30 15:02:48 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -29,13 +29,56 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const profileCloseModal = document.querySelector("#profile-close-modal");
+const profileEditCloseButton = profileEditModal.querySelector(
+  "#profile-close-modal"
+);
+const profileTitle = document.querySelector(".profile__title");
+const profileDesciption = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
+// functions
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+// event handlers
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDesciption.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+//event listeners
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDesciption.textContent;
   profileEditModal.classList.add("modal__opened");
 });
 
-profileCloseModal.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
+profileEditCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  // clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(true);
+  //access the card title and image and store them in variables
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardDescriptionTextEl = cardElement.querySelector(".card__title");
+  //set the path to the image to the link field of the object
+
+  //set the image alt text to the name field of the object
+  //set the card title to the name field of the object, too
+  cardDescriptionTextEl.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data
+  // return cardElement;
 });
+
 
