648e1aa6dd2fedbe852234d8b981f4337013274e
XDRO <131299160+XDRO@users.noreply.github.com>
2023-09-14 10:04:10 -0400

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -36,22 +36,50 @@ export default class Card {
     this._cardElement.remove();
   }
 
-  getView() {
-    this._cardElement = document
+  getElement() {
+    return document
       .querySelector(this._cardSelector)
       .content.querySelector(".card")
       .cloneNode(true);
-    // select the image element
-    this._cardImage = this._cardElement.querySelector(".card__image");
-    this._cardImage.src = this._link;
-    this._cardText = this._cardElement.querySelector(".card__description-text");
-    this._cardText.textContent = this._name;
-    this._cardPreviewImage = this._cardElement.querySelector(
-      "#preview-image-modal"
+  }
+
+  // getElement() {
+  //   this._cardElement = document
+  //     .querySelector(this._cardSelector)
+  //     .content.querySelector(".card")
+  //     .cloneNode(true);
+  // }
+
+  getView() {
+    this._cardElement = this.getElement();
+    const cardTitle = this._cardElement.querySelector(
+      ".card__description-text"
     );
-    // cardDescriptionTextEl.textContent = ;
-    // add src and alt
+    const cardImage = this._cardElement.querySelector(".card__image");
+    cardImage.src = this._link;
+    cardImage.alt = this._name;
+    cardTitle.textContent = this._name;
     this._setEventListeners();
     return this._cardElement;
   }
 }
+
+//   getView() {
+//     this._cardElement = document
+//       .querySelector(this._cardSelector)
+//       .content.querySelector(".card")
+//       .cloneNode(true);
+//     // select the image element
+//     this._cardImage = this._cardElement.querySelector(".card__image");
+//     this._cardImage.src = this._link;
+//     this._cardText = this._cardElement.querySelector(".card__description-text");
+//     this._cardText.textContent = this._name;
+//     this._cardPreviewImage = this._cardElement.querySelector(
+//       "#preview-image-modal"
+//     );
+//     // cardDescriptionTextEl.textContent = ;
+//     // add src and alt
+//     this._setEventListeners();
+//     return this._cardElement;
+//   }
+// }
