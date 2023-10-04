5b1f266e012838f6fad9379b2f1207ba77d4d153
XDRO <joshuaarcher76@gmail.com>
2023-10-04 09:51:50 -0400

    refactor section.js

--- src/components/scripts/Section.js
+++ src/components/scripts/Section.js
@@ -2,14 +2,14 @@ export default class Section {
   constructor({ items, renderer }, container) {
     this._items = items;
     this._renderer = renderer;
-    this._container = document.querySelector(container);
+    this._container = document.querySelectorAll(".modal__container");
   }
   renderItems() {
     this._items.forEach((item) => {
       this._renderer(item);
     });
   }
-  addItem(element) {
-    this._container.prepend(element);
+  addItem() {
+    this._container.prepend();
   }
 }
