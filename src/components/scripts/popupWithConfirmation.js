f93a84efa86038f4ebe9330c80e79c6100c1e66f
XDRO <joshuaarcher76@gmail.com>
2023-10-23 11:04:00 -0500

    refactor delete confirmation

--- src/components/scripts/popupWithConfirmation.js
+++ src/components/scripts/popupWithConfirmation.js
@@ -3,23 +3,30 @@ import Popup from "./Popup";
 export default class ConfirmationOnClick extends Popup {
   constructor({ popupSelector }) {
     super(popupSelector);
+    this._deleteConfirmForm = document.querySelector(popupSelector);
+    console.log(this._deleteConfirmForm);
     this._deleteConfirmPopup = this._popupElement.querySelector(".modal__form");
-    this._deleteConfirmForm = document.querySelector(
-      "#modal-delete-confirmation"
-    );
+    console.log(this._deleteConfirmPopup);
     this._deleteConfirmContainer = this._popupElement.querySelector(
       ".modal__delete-container"
     );
+    console.log(this._deleteConfirmContainer);
     this._deleteFormCloseButton = this._popupElement.querySelector(
       ".modal__close-button_delete-form"
     );
+    console.log(this._deleteFormCloseButton);
     this._deleteConfirmSubmit = this._popupElement.querySelector(
       ".modal__button-delete_save"
     );
+    console.log(this._deleteConfirmSubmit);
     this._deleteSubmitConfimText = this._deleteConfirmSubmit.textContent;
   }
 
-  open() {}
+  open() {
+    this._deleteConfirmForm.addEventListener("click", function () {
+      this._deleteConfirmForm.classList.add("modal_opened");
+    });
+  }
 
   setEventListeners() {
     this.open();
